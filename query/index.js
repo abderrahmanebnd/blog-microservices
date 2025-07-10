629a87c597af85eded414d6d1f4cab4a9f7caddf
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-07-10 18:57:33 +0100

    Parsing Incoming Events

--- query/index.js
+++ query/index.js
@@ -7,8 +7,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
-app.get("/events", (req, res) => {});
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.status(200).send({ posts });
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+
+    post.comments.push({ id, content });
+  }
+  console.log(posts);
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("query listens in 4002");
